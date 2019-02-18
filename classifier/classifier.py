c4f053334a308b36c4282db5fa577238a8875cd3
Ed Chalstrey <echalstrey@MAC-ATI0379.local>
2019-02-18 14:08:37 +0000

    playing with a different dataset

--- classifier/classifier.py
+++ classifier/classifier.py
@@ -1,30 +1,30 @@
 from sklearn import datasets, svm, metrics
 import time
 
-digits = datasets.load_digits()
-
-n_samples = len(digits.images)
-data = digits.images.reshape((n_samples, -1))
-
-expected = digits.target[n_samples // 2:]
-
-classifier = svm.SVC(gamma='scale')
-
-start = time.time()
-classifier.fit(data[:n_samples // 2], digits.target[:n_samples // 2])
-end = time.time()
-training_time = end - start
-
-start = time.time()
-predicted = classifier.predict(data[n_samples // 2:])
-end = time.time()
-classifier_time = end - start
-
-report = metrics.classification_report(expected, predicted, output_dict=True)
-
-performance = report['micro avg']['f1-score']
-
 def results():
+
+    digits = datasets.load_digits()
+
+    n_samples = len(digits.images)
+    data = digits.images.reshape((n_samples, -1))
+
+    expected = digits.target[n_samples // 2:]
+
+    classifier = svm.SVC(gamma='scale')
+
+    start = time.time()
+    classifier.fit(data[:n_samples // 2], digits.target[:n_samples // 2])
+    end = time.time()
+    training_time = end - start
+
+    start = time.time()
+    predicted = classifier.predict(data[n_samples // 2:])
+    end = time.time()
+    classifier_time = end - start
+
+    report = metrics.classification_report(expected, predicted, output_dict=True)
+
+    performance = report['micro avg']['f1-score']
 
     return({"Training time (s)": training_time, "Prediction time (s)": classifier_time,
     "Performance (micro avg f1 score)": report['micro avg']['f1-score']})
